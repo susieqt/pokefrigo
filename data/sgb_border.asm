ecd8c936377f05fcb05e2d8a8d12eb32cb14286d
susieqt <susieqt25@gmail.com>
2020-10-24 16:59:01 -0400

    new sgb border that isnt ugly

--- data/sgb_border.asm
+++ data/sgb_border.asm
@@ -4,7 +4,7 @@ BorderPalettes:
 	ds $100
 
 	RGB 24,  6,  6
-	RGB 31, 25,  9
+	RGB 15, 25, 15
 	RGB 25, 10, 10
 	RGB  0,  0,  0
 
@@ -24,8 +24,8 @@ BorderPalettes:
 	RGB 31, 31, 25
 
 	RGB 18, 24, 18
-	RGB 25, 10, 10
-	RGB 15, 25, 15
+	RGB 20, 20, 31
+	RGB 10, 10, 25
 	RGB  0,  0,  0
 
 	RGB 31, 31, 25
@@ -45,7 +45,7 @@ BorderPalettes:
 
 	RGB 18,  6, 31
 	RGB 31, 25,  9
-	RGB 15, 25, 15
+	RGB 25, 10, 10
 	RGB  0,  0,  0
 
 	RGB 31, 31, 25
